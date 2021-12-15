de851c1d0edb0da51e1d3a6b766f51f0ee6166fc
Moxvallix <moxvallix@gmail.com>
2021-12-15 21:58:19 +1030

    Add version text to bottom

--- src/ui_elements/mainmenu.gd
+++ src/ui_elements/mainmenu.gd
@@ -1,26 +1,26 @@
 extends Control
 
 # --Variables--
-enum MenuType {MAIN, CREATE, JOIN, SERVER}
+enum MenuType {MAIN, JOIN, CREATE, SERVER}
 
 onready var main_menu: Control = $MainMenu
-onready var create_menu: Control = $Create
 onready var join_menu: Control = $Join
+onready var create_menu: Control = $Create
 onready var server_menu: Control = $Server
 
 # --Interface--
 func _ready():
 	set_Visible_Menu(MenuType.MAIN)
-	
+
 func _on_Back_pressed():
 	set_Visible_Menu(MenuType.MAIN)
+
+func _on_GameJoinButton_pressed() -> void:
+	set_Visible_Menu(MenuType.JOIN)
 
 func _on_GameCreateButton_pressed() -> void:
 	set_Visible_Menu(MenuType.CREATE)
 
-func _on_GameJoinButton_pressed() -> void:
-	set_Visible_Menu(MenuType.JOIN)
-	
 func _on_ServerStartButton_pressed() -> void:
 	set_Visible_Menu(MenuType.SERVER)
 
@@ -31,21 +31,21 @@ func set_Visible_Menu(menu) -> void:
 	hide_Menus()
 	match menu:
 		MenuType.MAIN: main_menu.visible = true
-		MenuType.CREATE: create_menu.visible = true
 		MenuType.JOIN: join_menu.visible = true
+		MenuType.CREATE: create_menu.visible = true
 		MenuType.SERVER: server_menu.visible = true
 
 func hide_Menus() -> void:
 	main_menu.visible = false
-	create_menu.visible = false
 	join_menu.visible = false
+	create_menu.visible = false
 	server_menu.visible = false
 
 # --Backend--
-func _on_Create_pressed() -> void:
-	assert(false, "Game start not implemented yet")
-
 func _on_Join_pressed() -> void:
+	assert(false, "Game start not implemented yet")
+	
+func _on_Create_pressed() -> void:
 	assert(false, "Game start not implemented yet")
 
 func _on_Server_pressed():
