529651b1183088143ddd29a3fee68b7bf336418a
Moxvallix <moxvallix@gmail.com>
2021-12-16 10:54:18 +1030

    Adds accent colours, enter to join

--- src/ui_elements/mainmenu.gd
+++ src/ui_elements/mainmenu.gd
@@ -3,6 +3,8 @@ extends Control
 # --Variables--
 enum MenuType {MAIN, JOIN, CREATE, SERVER}
 
+var menu
+
 onready var main_menu: Control = $MainMenu
 onready var join_menu: Control = $Join
 onready var create_menu: Control = $Create
@@ -10,19 +12,28 @@ onready var server_menu: Control = $Server
 
 # --Interface--
 func _ready():
-	set_Visible_Menu(MenuType.MAIN)
+	menu = MenuType.MAIN
+	set_Visible_Menu(menu)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		join_Event(menu)
 
 func _on_Back_pressed():
-	set_Visible_Menu(MenuType.MAIN)
+	menu = MenuType.MAIN
+	set_Visible_Menu(menu)
 
 func _on_GameJoinButton_pressed() -> void:
-	set_Visible_Menu(MenuType.JOIN)
+	menu = MenuType.JOIN
+	set_Visible_Menu(menu)
 
 func _on_GameCreateButton_pressed() -> void:
-	set_Visible_Menu(MenuType.CREATE)
+	menu = MenuType.CREATE
+	set_Visible_Menu(menu)
 
 func _on_ServerStartButton_pressed() -> void:
-	set_Visible_Menu(MenuType.SERVER)
+	menu = MenuType.SERVER
+	set_Visible_Menu(menu)
 
 func _on_AppQuitButton_pressed() -> void:
 	get_tree().quit()
@@ -42,11 +53,18 @@ func hide_Menus() -> void:
 	server_menu.visible = false
 
 # --Backend--
+func join_Event(menu):
+	match menu:
+		MenuType.MAIN: pass
+		MenuType.JOIN: assert(false, "Game start not implemented yet")
+		MenuType.CREATE: assert(false, "Game start not implemented yet")
+		MenuType.SERVER: assert(false, "Game start not implemented yet")
+
 func _on_Join_pressed() -> void:
-	assert(false, "Game start not implemented yet")
+	join_Event(menu)
 	
 func _on_Create_pressed() -> void:
-	assert(false, "Game start not implemented yet")
+	join_Event(menu)
 
 func _on_Server_pressed():
-	assert(false, "Game start not implemented yet")
+	join_Event(menu)
