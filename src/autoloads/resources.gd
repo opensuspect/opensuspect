7d68e68ab40eb96636546cc06e8c04e849865fe9
Moxvallix <moxvallix@gmail.com>
2021-12-20 14:40:10 +1030

    Implement changes suggested by the2ndreal0

--- src/autoloads/resources.gd
+++ src/autoloads/resources.gd
@@ -4,8 +4,8 @@ extends Node
 ## Example: get a list of resources in a set of directories
 ## Dictionary of directories, with the namespace, followed by the directory path:
 #	var dirs: Dictionary = {
-#	"Resource 1": "res://examples/resource1",
-#	"Resource 2": "res://examples/resource2"
+#	"Folder 1": "res://examples/folder1",
+#	"Folder 2": "res://examples/folder2"
 #	}
 #
 ## Array of file types to use (only files with these extensions will be listed):
@@ -13,28 +13,27 @@ extends Node
 #
 ## Pass the two variables into the list function:
 #	Resources.list(dirs, file_types)
-#	> {Resource 1:{Test 1:test_1.png, Test 2:test_2.svg}, Resource 2:{Test 3:test_3.svg}}
+#	> {Folder 1:{Test 1:test_1.png, Test 2:test_2.svg}, Folder 2:{Test 3:test_3.svg}}
 #
-## Select only resources in "Resource 1":
-#	Resources.list(dirs, file_types)["Resource 1"]
+## Select only resources in "Folder 1":
+#	Resources.list(dirs, file_types)["Folder 1"]
 #	> {Test 1:test_1.png, Test 2:test_2.svg}
 
 # --Public Functions--
 
 # Returns a dictionary of all resources from a given directory dictionary
-func list(directories: Dictionary, types: Array) -> Dictionary:
-	var resources: Dictionary = {} # Initialise resources dictionary
+func list(directories: Dictionary, types: PoolStringArray) -> Dictionary:
+	var resources: Dictionary = {}
 	for folder in directories.keys(): # Iterate over each folder specified, by their namespace (key)
 		var files = _listFilesInDirectory(directories[folder], types) # List files in each folder
 		# Add each file to the output dictionary
-		for file in files :
-			resources = _filesToDictionary(resources, folder, file, directories[folder], types)
+		resources[folder] = _filesToDictionary(folder, files, directories[folder], types)
 	return(resources) # Return the dictionary of namespaced resources
 
 # --Private Functions--
 
 # Lists all files in a path, that have the correct file extensions
-func _listFilesInDirectory(path: String, types: Array) -> Array:
+func _listFilesInDirectory(path: String, types: PoolStringArray) -> Array:
 	var files: Array # Defunes the array to be returned
 	var dir = Directory.new() # Makes a new directory object
 	dir.open(path) # Opens the directory given in "path"
@@ -47,20 +46,20 @@ func _listFilesInDirectory(path: String, types: Array) -> Array:
 			for type in types: # Iterates over each specified file extension
 				if file.ends_with(type): # Checks if the file has the extension
 					files.append(file) # If it does, add it to the files array
-	return files # Return the files array
+	return(files) # Return the files array
 
 # Creates a dictionary of the files in a directory
-func _filesToDictionary(resources: Dictionary, namespace: String, file: String, path: String, types: Array) -> Dictionary:
-	var resource: String # Defines the resource string 
-	for type in types: # Iterates over each specified file extension
-		if file.ends_with(type): # Checks if the file has the extension
-			resource = file.trim_suffix(type) # If it does, remove the extension, and set this as "resource"
-	resource = _formatString(resource) # Format the resource string for use in game
-	path = path + "/" + file # Create the full file path to the resource
-	if not resources.has(namespace): # Initialise the resource dictionary to have the namespace
-		resources[namespace] = {}
-	resources[namespace][resource] = path # Set the path in the resources dictionary
-	return(resources) # Return the resources dictionary
+func _filesToDictionary(namespace: String, files: PoolStringArray, path: String, types: PoolStringArray) -> Dictionary:
+	var output: Dictionary = {} # Defines the dictionary to output the files
+	var resource: String = "" # Defines the resource string 
+	for file in files:
+		for type in types: # Iterates over each specified file extension
+			if file.ends_with(type): # Checks if the file has the extension
+				resource = file.trim_suffix(type) # If it does, remove the extension, and set this as "resource"
+		resource = _formatString(resource) # Format the resource string for use in game
+		path = path + "/" + file # Create the full file path to the resource
+		output[resource] = path # Set the path in the resources dictionary
+	return(output) # Return the output dictionary
 
 # Formats a string to remove any special characters
 func _formatString(string: String) -> String:
