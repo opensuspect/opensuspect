e926ecd12a7bc6a0201e27c49aa7d205b0366f19
nicemicro <63175285+nicemicro@users.noreply.github.com>
2022-01-03 14:51:19 +0900

    implemented spawn point handling

--- src/game/game.gd
+++ src/game/game.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var spawnList: Array = [] # Storing spawn positions for current map
+var spawnCounter: int = 0 # A counter to take care of where characters spawn
+
 onready var mapNode: Node2D = $Map
 onready var characterNode: Node2D = $Characters
 onready var rng: RandomNumberGenerator = RandomNumberGenerator.new()
@@ -11,18 +14,43 @@ func loadMap(mapPath: String) -> void:
 	## Load map and place it on scene tree
 	var mapToLoad: Node = ResourceLoader.load(mapPath).instance()
 	mapNode.add_child(mapToLoad)
+	## Save spawn positions from the map
+	var spawnPosNode: Node = mapNode.get_child(0).get_node("SpawnPositions")
+	spawnList = []
+	for posNode in spawnPosNode.get_children():
+		spawnList.append(posNode.position)
+	## Spawn characters at spawn points
+	spawnAllCharacters()
 
 func addCharacter(networkId: int) -> void:
 	## Create character resource
 	var newCharacterResource: CharacterResource = Characters.createCharacter(networkId)
 	## Get character node reference
 	var newCharacter: KinematicBody2D = newCharacterResource.getCharacterNode()
-	## Randomize position
-	var characterPosition: Vector2
-	characterPosition.x = rng.randi_range(100, 500)
-	characterPosition.y = rng.randi_range(100, 500)
+	## Spawn the character
+	spawnCharacer(newCharacterResource)
 	characterNode.add_child(newCharacter) ## Add node to scene
-	newCharacterResource.setPosition(characterPosition) ## Apply position
+
+# These functions place the character on the map, but if it is a client, it will
+# be overwritten by the position syncing. It is done only so that the characters
+# are placed to a sane position no matter the network lag.
+func spawnAllCharacters() -> void:
+	## Reset spawn position counter
+	spawnCounter = 0
+	## Loop through all characters
+	for character in Characters.getCharacterResources():
+		spawnCharacer(character) ## Set spawn position
+
+func spawnCharacer(character: CharacterResource) -> void:
+	## Set character position
+	character.setPosition(spawnList[spawnCounter])
+	## Step spawn position counter
+	spawnCounter += 1
+	if spawnCounter > len(spawnList):
+		spawnCounter = 0
 
 func showStartButton(buttonShow: bool = true) -> void:
 	$CanvasLayer/GameStart.visible = buttonShow
+
+func _on_GameStart_pressed() -> void:
+	assert(false, "Not implemented yet")
