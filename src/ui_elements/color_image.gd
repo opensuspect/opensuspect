98030ef37018dd6ce5819fd1b19d38e049dace84
Moxvallix <moxvallix@gmail.com>
2022-01-12 13:19:12 +1030

    Modified to match UI

--- src/ui_elements/color_image.gd
+++ src/ui_elements/color_image.gd
@@ -12,13 +12,13 @@ func _draw():
 
 # draw a circle on top of the color gradient to show what color is currently selected
 func _drawColorPointer():
-	var blackCircleRadius: float = 15.0
-	var colorCircleRadius: float = 10.0
+	var outlineCircleRadius: float = 16.0
+	var colorCircleRadius: float = 12.0
 	var currentScale: Vector2 = scale
 	# counteract the scaling done to this node so the circle doesn't turn out stretchy
 	draw_set_transform(Vector2(0, 0), 0, Vector2(1 / scale.x, 1 / scale.y))
 	# draw the black circle
-	draw_circle(selectedColorPosition, blackCircleRadius, Color(0, 0, 0))
+	draw_circle(selectedColorPosition, outlineCircleRadius, Color("FDFE72")) # Standard yellow highlight color
 	# draw the colored circle over the black circle
 	draw_circle(selectedColorPosition, colorCircleRadius, selectedColor)
 	# set the scale back to what it was before (probably Vector2(1, 1))
