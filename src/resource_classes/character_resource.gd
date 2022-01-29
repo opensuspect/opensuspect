6d598013613fcc52413b35a67d573815d7af6abe
nicemicro <63175285+nicemicro@users.noreply.github.com>
2022-01-29 22:49:07 +0900

    Code revision with suggestions by TheSecondReal0
     * The setMainCharacter function in the characternode now takes a bool
       argument
     * The character_resource now stores whether it is the main character

--- src/resource_classes/character_resource.gd
+++ src/resource_classes/character_resource.gd
@@ -14,6 +14,7 @@ var networkId: int = -1 setget setNetworkId, getNetworkId
 # the name of this player
 var characterName: String
 
+var mainCharacter: bool = false
 # --Private Variables--
 
 # the character node corresponding to this CharacterResource
@@ -76,6 +77,7 @@ func setCharacterNode(newCharacterNode: Node) -> void:
 	_characterNode = newCharacterNode
 	if networkId == Connections.getMyId():
 		_characterNode.setMainCharacter()
+		mainCharacter = true
 
 # get the role of this character
 # string is PLACEHOLDER
