0412ed5d52ccbb97441f9d8f5e085c070ae7de46
nicemicro <63175285+nicemicro@users.noreply.github.com>
2022-02-17 08:31:17 +0900

    interactive role announcement screen dummy

--- src/game/role_assignment.gd
+++ src/game/role_assignment.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+onready var teamNameField: Label = $LabelTeamName
+onready var roleNameField: Label = $LabelRoleName
+
 func _ready():
 	TransitionHandler.gameScene.connect("teamsRolesAssigned", self, "showTeamsRoles")
 
 func showTeamsRoles(roles: Dictionary) -> void:
-	print_debug(roles)
+	var id: int = get_tree().get_network_unique_id()
+	teamNameField.text = roles[id]["team"]
+	roleNameField.text = roles[id]["role"]
+	# TODO: add character sprites to the CharacterSprites BoxContainer
